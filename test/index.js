a94f026a58fb7e8624e1936836b8364faad617c5
qinhaojie <qinhaojie1992@163.com>
2016-06-22 13:34:43 +0800

    util新增copyDir 改写askByQuesList

--- test/index.js
+++ test/index.js
@@ -1,7 +1,13 @@
-const askByQuesList = require('../lib/util.js').askByQuesList
+const util = require('../lib/util.js')
 
-askByQuesList([
-    '1','2','3'
+util.askByQuesList([
+    ['aaa','a'],['bbb','b'],['ccc','c']
 ]).then(a =>{
     console.log(a)
 })
+
+// util.copyDir('./lib','./aaa').then(()=>{
+//     setTimeout(function() {
+//         require('fs-promise').remove('./aaa')
+//     }, 2000);
+// })
